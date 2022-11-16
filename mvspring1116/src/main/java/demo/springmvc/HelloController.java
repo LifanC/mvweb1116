a1497205bef1dc1d47c67d56e4bb7d586efbf5e3
student <student@DESKTOP-4ARRLB2>
2022-11-16 16:15:46 +0800

    Spring MVC Replenish

--- mvspring1116/src/main/java/demo/springmvc/HelloController.java
+++ mvspring1116/src/main/java/demo/springmvc/HelloController.java
@@ -3,6 +3,8 @@ package demo.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
  
 @Controller
 public class HelloController {
@@ -12,5 +14,22 @@ public class HelloController {
         model.addAttribute("greeting", "Hello Spring MVC");         
         return"helloworld";         
     }
+    @RequestMapping(value = "/redirect", method = RequestMethod.GET)
+    public String authorInfo(Model model) {        
+        return "redirect:/hello";
+    }
+    @RequestMapping("/user")
+    public String userInfo(Model model,
+            @RequestParam(value = "name", defaultValue = "Guest") String name) {
  
+        model.addAttribute("name", name);
+ 
+        if("admin".equals(name)) {
+            model.addAttribute("email", "admin@demo.com");
+        } else{
+            model.addAttribute("email", "Not set");
+        }
+        return "userInfo";
+    }
+
 }
